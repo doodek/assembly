108544743937c0ba969f44e7e72e81d70ecf30da
Bartlomiej <bartlomiej.dudek.3@gmail.com>
2021-05-15 17:43:16 +0200

    Added bootable hello-world.

---
+++ hello_world/boot.asm
@@ -0,0 +1,24 @@
+org 0x7c00
+bits 16
+
+start:
+    cli
+    mov si, msg
+    mov ah, 0x0e
+.loop: 
+    lodsb
+    cmp al, 0
+    je end
+    int 0x10
+    jmp .loop
+
+end:
+    hlt
+msg:
+    db "Hello, World!", 0
+
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
+
+
